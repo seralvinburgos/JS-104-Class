ad54b33bb66363a7efc490820a2ff304e5a7d3af
seralvinburgos <110437441+seralvinburgos@users.noreply.github.com>
2022-08-29 23:32:57 -0400

    register - updated js
    updated js array elements.

--- Pet Saloon Class/scripts/register.js
+++ Pet Saloon Class/scripts/register.js
@@ -1,4 +1,13 @@
-console.log("Registering");
+alert(4 + " pets are registered at this time");
+
+const petsNames=["Scooby","Scrappy","Siopao","Boba"];
+console.log(petsNames[0]);
+console.log(petsNames[1]);
+console.log(petsNames[2]);
+console.log(petsNames[3]);
+
+// console.table(petsNames);
+
 //object literal
 let petSaloon={
     //attributes
@@ -46,11 +55,10 @@ let petSaloon={
             service:"Grooming",
             owners:"EJ",
             phone:"888-888-8888"
-        },
+        }
     ] //pet's array
 }
 console.log(petSaloon.pets);
-console.log(petSaloon.address.street);
 
 function displaySaloonInfo(){
 document.getElementById("info").innerHTML=`
@@ -58,11 +66,19 @@ document.getElementById("info").innerHTML=`
 }
 
 function displayPetsNames(){
+document.getElementById("info").innerHTML=`
+    <p>${petSaloon.pets.name}</p>
+    <p>${petsNames[1]}</p>
+    <p>${petsNames[2]}</p>
+    <p>${petsNames[3]}</p>`;
+}
     // use alert to display the amount of pets (alert(),length)
 
     //travel the pets array
+for(let i=0;i<petsNames.length;i++) {
+    console.log(petsNames[i]);
+} 
 
     //display on the console -> display them on the HTML
-}
 
 displaySaloonInfo();
